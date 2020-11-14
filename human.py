4e7ec43bb24079b51d6c57aa6b7f4a27a89ee6bd
Don Charles-Lambert <zcabdc0@ucl.ac.uk>
2020-11-14 13:38:06 +0000

    Bible Verses and Investment Discussion

--- human.py
+++ human.py
@@ -1,8 +1,40 @@
-from data import quotes
+from data import verses, quotes, investing, bad_word, good_word
+from stock import random_stock, Stock
 import random
 
 def say_quote():
     return quotes[random.randint(0, len(quotes)-1)]
+
+def say_verse():
+    return verses[random.randint(0, len(verses)-1)]
+
+def discuss_investing():
+    stock = random_stock()
+    price = stock.get_recent_price()
+    yesterday_price = stock.get_yesterdays_price()
+    difference = round(float(price)-float(yesterday_price), 2)
+
+    speech = investing[random.randint(0, len(investing)-1)]
+    speech = speech.replace("[[name]]", stock.name)
+    speech = speech.replace("[[emoji]]", stock.emoji)
+    speech = speech.replace("[[price]]", '£' + price)
+    speech = speech.replace("[[raw_change]]", '£' + str(difference))
+    speech = speech.replace("[[yesterday]]", yesterday_price)
+
+    if difference > 0:
+        speech = speech.replace("[[up]]", 'up')
+    else:
+        speech = speech.replace("[[up]]", 'down')
+
+    if difference > 0:
+        speech = speech.replace("[[description]]", good_word())
+    else:
+        speech = speech.replace("[[description]]", bad_word())
+
+    return speech.capitalize()
+
+print(say_verse())
+
 
 
 
--- stock.py
+++ stock.py
@@ -4,6 +4,7 @@ from data import stock_data
 import numpy as np
 from sklearn import datasets, linear_model
 import datetime
+import random
 
 class Stock():
     def __init__(self, name):
@@ -18,22 +19,29 @@ class Stock():
         return times
 
     def get_yesterdays_price(self):
-        yesterday = datetime.datetime.now() - datetime.timedelta(1)
-        yesterstring = datetime.datetime.strftime(yesterday, '%Y-%m-%d')
         response = requests.get('https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={0}&apikey={1}'.format(self.symbol, keys.ALPHAVANTAGE)).json()
         times = response['Time Series (Daily)']
+
+        
+        yesterday = datetime.datetime.now() - datetime.timedelta(1)
+        yesterstring = datetime.datetime.strftime(yesterday, '%Y-%m-%d')
+        minus_days = 1
+        while yesterstring not in times:
+            minus_days += 1
+            yesterday = datetime.datetime.now() - datetime.timedelta(minus_days)
+            yesterstring = datetime.datetime.strftime(yesterday, '%Y-%m-%d')
         yesterdays_prices = times[yesterstring]
         return yesterdays_prices['4. close']
 
     def get_recent_price(self):
-        most_recent_time = list(self.times.keys())[len(self.times.keys())-1]
+        most_recent_time = list(self.times.keys())[0]
         current_price_list = self.times[most_recent_time]
         return current_price_list['4. close']
 
     def get_recent_n_prices(self, n):
         prices = []
-        for i in range(1, min(n+1, len(self.times.keys()))):
-            ith_recent_time = list(self.times.keys())[len(self.times.keys())-i]
+        for i in range(1, min(n+1, len(self.times.keys())-1)):
+            ith_recent_time = list(self.times.keys())[i]
             price = self.times[ith_recent_time]['4. close']
             prices.append(price)
         return prices
@@ -42,15 +50,11 @@ class Stock():
         y_train = np.array(self.get_recent_n_prices(5))
         x_train = np.array([i*15 for i in range(0, len(y_train))]).reshape((-1, 1))
         x_test = [x_train[len(x_train) - 1] + 15]
-        # Create linear regression object
         regr = linear_model.LinearRegression()
-
-        # Train the model using the training sets
         regr.fit(x_train, y_train)
-
-        # Make predictions using the testing set
         y_pred = regr.predict(x_test)
         return y_pred
 
-apple = Stock('Apple')
-print(apple.get_yesterdays_price())
+def random_stock():
+    name = list(stock_data.keys())[random.randint(0, len(stock_data.keys())-1)]
+    return Stock(name)
